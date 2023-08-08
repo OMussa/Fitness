dac3dad1bbd085e12425aec780ff5cfefa878dfc
OMussa <mussa_o@icloud.com>
2023-08-08 11:12:41 -0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+const Navbar = () => {
+  return (
+    <nav className='nav'>
+        <ul>
+            <li>WorkFits</li>
+            <li>log in</li>
+            <li>Plan</li>
+            <li>Contact</li>
+           \
+        </ul>
+        
+        
+        
+       
+       
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Workout.js
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+
+export default function Workout() {
+    return(
+        <div>
+            <Header/>
+            <h2>Workouts</h2>
+        </div>
+    )
+
+}
